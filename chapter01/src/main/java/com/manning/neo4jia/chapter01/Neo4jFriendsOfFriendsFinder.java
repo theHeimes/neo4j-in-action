c797989369e1664984862b3a5e39965aa89f3136
Thorsten Heimes <thorstentheimes@gmail.com>
2016-03-18 17:56:25 +0100

    Added transaction to FriendFinder.

--- chapter01/src/main/java/com/manning/neo4jia/chapter01/Neo4jFriendsOfFriendsFinder.java
+++ chapter01/src/main/java/com/manning/neo4jia/chapter01/Neo4jFriendsOfFriendsFinder.java
@@ -5,6 +5,7 @@ import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipExpander;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.graphdb.traversal.TraversalDescription;
 import org.neo4j.kernel.StandardExpander;
@@ -12,6 +13,7 @@ import org.neo4j.kernel.Traversal;
 import org.neo4j.kernel.Uniqueness;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StopWatch;
 
 import java.util.ArrayList;
@@ -22,93 +24,119 @@ import java.util.List;
  */
 public class Neo4jFriendsOfFriendsFinder implements FriendsOfFriendsFinder {
 
-    private static Logger logger = LoggerFactory.getLogger(Neo4jFriendsOfFriendsFinder.class);
+	private static Logger logger = LoggerFactory.getLogger(Neo4jFriendsOfFriendsFinder.class);
 
-    private final GraphDatabaseService graphDb;
+	private final GraphDatabaseService graphDb;
 
-    public Neo4jFriendsOfFriendsFinder(GraphDatabaseService graphDb) {
-        this.graphDb = graphDb;
-    }
+	public Neo4jFriendsOfFriendsFinder(GraphDatabaseService graphDb) {
+		this.graphDb = graphDb;
+	}
 
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
+	@Transactional
 	public Long countFriendsOfFriends(Long userId) {
-        Node nodeById = graphDb.getNodeById(userId);
+		
+		Long result = 0L;
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+		try (Transaction tx = graphDb.beginTx()) {
+			Node nodeById = graphDb.getNodeById(userId);
+			TraversalDescription traversalDescription = Traversal.description()
+					.relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(2))
+					.uniqueness(Uniqueness.NODE_GLOBAL);
+			Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
 
-        TraversalDescription traversalDescription = Traversal.description().relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(2)).uniqueness(Uniqueness.NODE_GLOBAL);
-        Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
-        Long result = 0L;
-        for (Node n : nodes) {
-            result++;
-        }
-        stopWatch.stop();
-        logger.info("NEO4J: Found {} friends of friends for user {}, took " + stopWatch.getTotalTimeMillis() + " millis.", result, userId);
+			for (Node n : nodes) {
+				result++;
+			}
+			stopWatch.stop();
+			logger.info("NEO4J: Found {} friends of friends for user {}, took " + stopWatch.getTotalTimeMillis()
+					+ " millis.", result, userId);
+		}
+		return result;
+	}
 
-        return result;
-    }
-
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
+	@Transactional
 	public Long countFriendsOfFriendsDepth3(Long userId) {
-        Node nodeById = graphDb.getNodeById(userId);
+		
+		Long result = 0L;
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 
-        TraversalDescription traversalDescription = Traversal.description().relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(3)).uniqueness(Uniqueness.NODE_GLOBAL);
-        Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
-        Long result = 0L;
-        for (Node n : nodes) {
-            result++;
-        }
-        stopWatch.stop();
-        logger.info("NEO4J: Found {} friends of friends depth 3 for user {}, took " + stopWatch.getTotalTimeMillis() + " millis.", result, userId);
+		try (Transaction tx = graphDb.beginTx()) {
+			Node nodeById = graphDb.getNodeById(userId);
+			TraversalDescription traversalDescription = Traversal.description()
+					.relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(3))
+					.uniqueness(Uniqueness.NODE_GLOBAL);
+			Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
 
-        return result;
-    }
+			for (Node n : nodes) {
+				result++;
+			}
+			stopWatch.stop();
+			logger.info("NEO4J: Found {} friends of friends depth 3 for user {}, took " + stopWatch.getTotalTimeMillis()
+					+ " millis.", result, userId);
+		}
+		return result;
+	}
 
-    public Long countFriendsOfFriendsDepth4(Long userId) {
-        Node nodeById = graphDb.getNodeById(userId);
+	@Transactional
+	public Long countFriendsOfFriendsDepth4(Long userId) {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Long result = 0L;
+		try (Transaction tx = graphDb.beginTx()) {
+			Node nodeById = graphDb.getNodeById(userId);
+			@SuppressWarnings("deprecation")
+			TraversalDescription traversalDescription = Traversal.description()
+					.relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(4))
+					.uniqueness(Uniqueness.NODE_GLOBAL);
+			Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
+			for (Node n : nodes) {
+				result++;
+			}
+			stopWatch.stop();
+			logger.info("NEO4J: Found {} friends of friends depth 4 for user {}, took " + stopWatch.getTotalTimeMillis()
+					+ " millis.", result, userId);
+		}
+		return result;
+	}
 
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
+	@Transactional
+	public Long countFriendsOfFriendsDepth5(Long userId) {
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Long result = 0L;
+		try (Transaction tx = graphDb.beginTx()) {
+			Node nodeById = graphDb.getNodeById(userId);
+			@SuppressWarnings("deprecation")
+			TraversalDescription traversalDescription = Traversal.description()
+					.relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(5))
+					.uniqueness(Uniqueness.NODE_GLOBAL);
+			Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
+			for (Node n : nodes) {
+				result++;
+			}
+			stopWatch.stop();
+			logger.info("NEO4J: Found {} friends of friends depth 5 for user {}, took " + stopWatch.getTotalTimeMillis()
+					+ " millis.", result, userId);
+		}
+		return result;
 
-        @SuppressWarnings("deprecation")
-		TraversalDescription traversalDescription = Traversal.description().relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(4)).uniqueness(Uniqueness.NODE_GLOBAL);
-        Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
-        Long result = 0L;
-        for (Node n : nodes) {
-            result++;
-        }
-        stopWatch.stop();
-        logger.info("NEO4J: Found {} friends of friends depth 4 for user {}, took " + stopWatch.getTotalTimeMillis() + " millis.", result, userId);
+	}
 
-        return result;
-    }
-
-    public Long countFriendsOfFriendsDepth5(Long userId) {
-        Node nodeById = graphDb.getNodeById(userId);
-
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-
-        @SuppressWarnings("deprecation")
-		TraversalDescription traversalDescription = Traversal.description().relationships(Constants.IS_FRIEND_OF, Direction.OUTGOING).evaluator(Evaluators.atDepth(5)).uniqueness(Uniqueness.NODE_GLOBAL);
-        Iterable<Node> nodes = traversalDescription.traverse(nodeById).nodes();
-        Long result = 0L;
-        for (Node n : nodes) {
-            result++;
-        }
-        stopWatch.stop();
-        logger.info("NEO4J: Found {} friends of friends depth 5 for user {}, took " + stopWatch.getTotalTimeMillis() + " millis.", result, userId);
-
-        return result;
-
-    }
-
-    @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 	public boolean areConnectedViaFriendsUpToLevel4(Long user1, Long user2) {
-        return GraphAlgoFactory.shortestPath((RelationshipExpander)StandardExpander.DEFAULT.add(Constants.IS_FRIEND_OF, Direction.OUTGOING), 4).findAllPaths(graphDb.getNodeById(user1), graphDb.getNodeById(user2)).iterator().hasNext();
-    }
+		try (Transaction tx = graphDb.beginTx()) {
+		return GraphAlgoFactory.shortestPath(
+				(RelationshipExpander) StandardExpander.DEFAULT.add(Constants.IS_FRIEND_OF, Direction.OUTGOING), 4)
+				.findAllPaths(graphDb.getNodeById(user1), graphDb.getNodeById(user2)).iterator().hasNext();
+		} catch( Exception e) {
+		return false;
+		}
+	}
 }
